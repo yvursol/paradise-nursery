fd5a45631aa7e96a3b60f98b5742016b1445888e
Yuri Vursol <yvursol@gmail.com>
2026-02-15 16:43:13 +0300

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,10 +1,5 @@
-const AboutUs = () => {
-  return (
-    <div>
-      <h2>About Paradise Nursery</h2>
-      <p>We provide the best indoor plants for your home and office.</p>
-    </div>
-  );
-};
-
-export default AboutUs;
+<div className="about-us-container">
+  <h2>About Paradise Nursery</h2>
+  <p>We bring greenery to your home...</p>
+  <p>Our mission is to make plants accessible to everyone.</p>
+</div>
