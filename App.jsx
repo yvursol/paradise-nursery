6bd690f39110440c21f2f4c121919287a7c76210
Yuri Vursol <yvursol@gmail.com>
2026-02-15 16:29:50 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing">
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
