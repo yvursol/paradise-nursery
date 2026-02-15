0fad9bfa818b770bd80b37d90f513c08799b6171
Yuri Vursol <yvursol@gmail.com>
2026-02-15 16:45:57 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,35 @@
-const [showProducts, setShowProducts] = useState(false);
+import React, { useState } from 'react';
+import './App.css';
+import ProductList from './ProductList';
+import CartItem from './CartItem';
 
-{!showProducts ? (
-  <div className="landing">
-    <h1>Paradise Nursery</h1>
-    <button onClick={() => setShowProducts(true)}>Get Started</button>
-  </div>
-) : (
-  <ProductList />
-)}
+function App() {
+  const [currentPage, setCurrentPage] = useState('landing');
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'products':
+        return <ProductList />;
+      case 'cart':
+        return <CartItem />;
+      default:
+        return (
+          <div className="landing">
+            <h1>Paradise Nursery</h1>
+            <p>Your one-stop shop for beautiful indoor plants</p>
+            <button onClick={() => setCurrentPage('products')}>
+              Get Started
+            </button>
+          </div>
+        );
+    }
+  };
+
+  return (
+    <div className="App">
+      {renderPage()}
+    </div>
+  );
+}
+
+export default App;
