261edd24a4407dfa965db88fa87166507becb48a
Yuri Vursol <yvursol@gmail.com>
2026-02-15 16:28:27 +0300

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide the best indoor plants for your home and office.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
